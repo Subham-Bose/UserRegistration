86ce5dce99361ba15b763fae37a0597d3729105c
Subham-Bose <70543789+Subham-Bose@users.noreply.github.com>
2024-10-08 01:02:45 +0530

    Added the basic files
    Add all the basic files related to authentication of a user.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserAuthentication.Models;
+using UserAuthentication.Services;
+
+namespace UserAuthentication.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // POST: auth/login
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginUser user)
+        {
+            if (String.IsNullOrEmpty(user.UserName))
+            {
+                return BadRequest(new { message = "Email address needs to entered" });
+            }
+            else if (String.IsNullOrEmpty(user.Password))
+            {
+                return BadRequest(new { message = "Password needs to entered" });
+            }
+
+            User loggedInUser = await _authService.Login(user.UserName, user.Password);
+
+            if (loggedInUser != null)
+            {
+                return Ok(loggedInUser);
+            }
+
+            return BadRequest(new { message = "User login unsuccessful" });
+        }
+
+        // POST: auth/register
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser user)
+        {
+            if (String.IsNullOrEmpty(user.Name))
+            {
+                return BadRequest(new { message = "Name needs to entered" });
+            }
+            else if (String.IsNullOrEmpty(user.UserName))
+            {
+                return BadRequest(new { message = "User name needs to entered" });
+            }
+            else if (String.IsNullOrEmpty(user.Password))
+            {
+                return BadRequest(new { message = "Password needs to entered" });
+            }
+
+            User userToRegister = new(user.UserName, user.Name, user.Password, user.Role);
+
+            User registeredUser = await _authService.Register(userToRegister);
+
+            User loggedInUser = await _authService.Login(registeredUser.UserName, user.Password);
+
+            if (loggedInUser != null)
+            {
+                return Ok(loggedInUser);
+            }
+
+            return BadRequest(new { message = "User registration unsuccessful" });
+        }
+    }
+}
---
+++ Repository/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAuthentication.Models;
+
+namespace UserAuthentication.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
